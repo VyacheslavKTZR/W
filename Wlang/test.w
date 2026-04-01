0a5680d4750251f3c2647e3b271a41be7b1cbe67
VyacheslavKTZR <katzer.sla@gmail.com>
2026-04-01 10:03:44 +0300

    надо удалить

--- Wlang/test.w
+++
@@ -1,5 +0,0 @@
-var.create x
-var.set x 42
-!cr x
-end
-run
--- Wlang/w.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-#include <string>
-#include <unordered_map>
-
-void executeCommand(const std::string& line,
-    std::unordered_map<std::string, int>& vars,
-    std::unordered_map<std::string, std::string>& stringVars);
